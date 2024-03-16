e8d046c7262d830ca2107b47d78c4b10ed663741
Abraham A. <91634097+realSynced@users.noreply.github.com>
2024-03-16 20:53:11 +0000

    Removed <Navbar/> from marketplace.jsx

--- nexus/src/app/marketplace/page.jsx
+++ nexus/src/app/marketplace/page.jsx
@@ -5,8 +5,7 @@ export default function Marketplace() {
 
 
   return (
-    <main className="min-h-screen">
-      <Navbar/>
+    <div className="min-h-screen">
       <section className={`${oxanium.className} flex h-dvh justify-center items-center text-center text-8xl`}>
           
           <div className={`mb-18`}>
@@ -22,6 +21,6 @@ export default function Marketplace() {
           </div>
       </section>
       
-    </main>
+    </div>
   );
 }
