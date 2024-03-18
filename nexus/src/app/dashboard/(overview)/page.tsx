48da0ee681a04bb7888dad2c00b33273029c8bb3
Abraham A <91634097+realSynced@users.noreply.github.com>
2024-03-18 03:08:22 +0000

    Made a check for null if session?.user.email is null

--- nexus/src/app/dashboard/(overview)/page.tsx
+++ nexus/src/app/dashboard/(overview)/page.tsx
@@ -4,7 +4,13 @@ import { useSession } from "next-auth/react"
 
 
 export default function Dashboard() {
-  const { data: session, status } = useSession()
+  const { data: session, status } = useSession();
+
+  const email = () => {
+    if(session?.user.email === null){
+      return;
+    }
+  }
 
   // const session = await getSession({req})
     // const userInfo = await getUserInfo();
