39a1ba737af51389a4c03bab9a13755751103c33
Abraham A <91634097+realSynced@users.noreply.github.com>
2024-03-18 03:31:54 +0000

    Fixed minor issue in /login, with a return and some props.

--- nexus/src/app/accounts/login/page.tsx
+++ nexus/src/app/accounts/login/page.tsx
@@ -10,7 +10,7 @@ export default async function LoginPage() {
   if(session){
     
     redirect("/dashboard");
-    return (<Navbar state={session} />)
+    // return (<Navbar state={session} />)
   }
   return (<>
     {/* <Navbar/> */}
