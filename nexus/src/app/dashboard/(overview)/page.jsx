e5c95bb8a03726f1fa71817708f3f0a6bf7a3020
Abraham A. <91634097+realSynced@users.noreply.github.com>
2024-03-20 23:07:46 +0000

    Fixed a bit of error/wrong code in dashboard (overview), and created server directory in root directory.

---
+++ nexus/src/app/dashboard/(overview)/page.jsx
@@ -0,0 +1,24 @@
+import { Console } from 'console';
+import dynamic from 'next/dynamic'
+
+
+
+export default function Dashboard() {
+  
+
+  return (
+    <div className="rounded-lg space-y-6 p-10 pb-16 md:block md:h-svh bg-gradient-to-tr from-white to-gray-200">
+      <div className="space-y-0.5">
+        <h2 className="text-2xl font-bold tracking-tight">Dashboard</h2>
+        <p className="text-muted-foreground">
+          Welcome! {/* {user.name} */}
+        </p>
+      </div>
+
+      <div className="shrink-0 bg-border h-[1px] w-full"></div>
+      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+        {/* Navigation links */}
+      </div>
+    </div>
+  );
+}
--- nexus/src/app/ui/navbar.tsx
+++ nexus/src/app/ui/navbar.tsx
@@ -1,5 +1,4 @@
-'use client'
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Link from "next/link";
 import { oxanium } from "@/app/ui/fonts";
 import { getServerSession } from "next-auth";
@@ -7,75 +6,52 @@ import Logout from "@/app/ui/logout";
 import RegSign from "@/app/ui/reg-sign";
 import styles from "@/app/ui/styles.module.css";
 
-export default function Navbarr() {
-  const [session, setSession] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const sessionData = await getServerSession();
-      if(sessionData == null){
-        setSession(true);
-      } else {
-        setSession(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-
+export default async function Navbarr() {
+  const session = await getServerSession();
   return (
+    
     <div className={`${oxanium.className} sticky top-4 z-50 px-12`}>
       <div className="">
         <nav className="rounded-full px-12 bg-white bg-opacity-70 border border-black">
-          <div className="flex items-center">
+          <div className="flex  items-center ">
             <div className="px-4 py-4">
               <Link href="/" className={`${styles.arrow} font-bold flex`}>
                 Nexus
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-4 h-6 ml-0 duration-300 transition-transform ease-in-out"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-                  />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
+                        className="w-4 h-6 ml-0 duration-300 transition-transform ease-in-out">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
                 </svg>
-              </Link>
+                
+                </Link>
             </div>
 
             <div className="px-4 py-4 ml-auto">
-              <Link href="/" className="font-bold">
-                Nexus
-              </Link>
+              <Link href="/" className="font-bold">Nexus</Link>
             </div>
-
+            
             <div className="px-4 py-4">
-              <Link href="/" className="font-bold">
-                Nexus
-              </Link>
+              <Link href="/" className="font-bold">Nexus</Link>
             </div>
             <div className="px-4 py-4">
-              <Link href="/" className="font-bold">
-                Nexus
-              </Link>
+              <Link href="/" className="font-bold">Nexus</Link>
             </div>
             <div className="px-4 py-4">
-              <Link href="/" className="font-bold">
-                Learn
-              </Link>
+              <Link href="/" className="font-bold">Learn</Link>
             </div>
-
-            {!!session && <RegSign />}
-            {!session && <Logout />}
+            
+            {/* <nav> */}
+            {!session &&
+              <RegSign/>
+            }
+            {!!session &&
+              <Logout/>
+            }
           </div>
         </nav>
       </div>
     </div>
+
+
   );
 }
